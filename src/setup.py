d80829645299a3b6adf8042b1661cc446a913ded
EfranH25 <efranhimel25@gmail.com>
2025-12-23 17:40:59 -0500

    added setup script

---
+++ src/setup.py
@@ -0,0 +1,29 @@
+import yaml
+from langchain_ollama import OllamaLLM
+
+
+def get_llm(config_location="configs/config.yaml") -> OllamaLLM:
+    # llm_model = OllamaLLM()
+    with open(config_location) as stream:
+        try:
+            config = yaml.safe_load(stream)
+        except yaml.YAMLError as exc:
+            raise f"Error parsing config YAML: {exc}"
+
+    llm_model = OllamaLLM(
+        model=config["ollama_config"]["model"],
+        base_url=config["ollama_config"]["url"],
+        **config["model_params"]
+    )
+
+    return llm_model
+
+
+
+if __name__ == "__main__":
+
+
+    model = get_llm()
+
+    result = model.invoke("what day is today?")
+    print(result)
