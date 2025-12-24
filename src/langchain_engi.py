a5e1c807fd6ed68997c86ca0dae7a6c30735b4d3
EfranH25 <efranhimel25@gmail.com>
2025-12-24 02:07:00 -0500

    added embedding model

--- src/langchain_engi.py
+++
@@ -1,8 +0,0 @@
-from src.setup import get_llm
-
-
-if __name__ == "__main__":
-    llm = get_llm()
-
-    resp = llm.invoke("what day is today")
-    print(resp)
--- src/setup.py
+++ src/setup.py
@@ -1,9 +1,8 @@
 import yaml
-from langchain_ollama import OllamaLLM
+from langchain_ollama import OllamaLLM, OllamaEmbeddings
 
 
 def get_llm(config_location="configs/config.yaml") -> OllamaLLM:
-    # llm_model = OllamaLLM()
     with open(config_location) as stream:
         try:
             config = yaml.safe_load(stream)
@@ -11,16 +10,32 @@ def get_llm(config_location="configs/config.yaml") -> OllamaLLM:
             raise f"Error parsing config YAML: {exc}"
 
     llm_model = OllamaLLM(
-        model=config["ollama_config"]["model"],
+        model=config["ollama_config"]["llm_model"],
         base_url=config["ollama_config"]["url"],
         **config["model_params"],
     )
-
     return llm_model
+
+def get_embed(config_location="configs/config.yaml"):
+    with open(config_location) as stream:
+        try:
+            config = yaml.safe_load(stream)
+        except yaml.YAMLError as exc:
+            raise f"Error parsing config YAML: {exc}"
+
+    embed = OllamaEmbeddings(
+        model=config["ollama_config"]["embedding_model"],
+        base_url=config["ollama_config"]["url"],
+    )
+
+    return embed
 
 
 if __name__ == "__main__":
-    model = get_llm()
+    # model = get_llm()
+    # result = model.invoke("what day is today?")
+    # print(result)
 
-    result = model.invoke("what day is today?")
-    print(result)
+    embedding_model = get_embed()
+    resp = embedding_model.embed_query("hello world")
+    print(resp)
