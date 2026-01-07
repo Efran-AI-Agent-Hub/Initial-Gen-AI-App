66cc256ded0de8dc2d7d5cad58aeaf8bad566c96
EfranH25 <efranhimel25@gmail.com>
2026-01-07 00:16:50 -0500

    added agent chat

--- src/setup.py
+++ src/setup.py
@@ -1,6 +1,20 @@
 import yaml
-from langchain_ollama import OllamaLLM, OllamaEmbeddings
+from langchain_ollama import OllamaLLM, OllamaEmbeddings, ChatOllama
 
+
+def get_chat_llm(config_location="configs/config.yaml") -> ChatOllama:
+    with open(config_location) as stream:
+        try:
+            config = yaml.safe_load(stream)
+        except yaml.YAMLError as exc:
+            raise f"Error parsing config YAML: {exc}"
+
+    llm_model = ChatOllama(
+        model="qwen3:4b",
+        base_url=config["ollama_config"]["url"],
+        **config["model_params"],
+    )
+    return llm_model
 
 def get_llm(config_location="configs/config.yaml") -> OllamaLLM:
     with open(config_location) as stream:
@@ -33,10 +47,14 @@ def get_embed(config_location="configs/config.yaml"):
 
 
 if __name__ == "__main__":
-    # model = get_llm()
-    # result = model.invoke("what day is today?")
-    # print(result)
+    model = get_llm()
+    resp = model.invoke("what day is today?")
+    print(resp)
 
     embedding_model = get_embed()
     resp = embedding_model.embed_query("hello world")
+    print(resp)
+
+    model = get_chat_llm()
+    resp = model.invoke("what day is today?")
     print(resp)
